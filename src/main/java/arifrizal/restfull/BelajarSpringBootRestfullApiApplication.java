6331d4fe53d0db7e159a15e7ffa5bae39e6f8abd
arif-rizal1122 <rizallarif32@gmail.com>
2024-12-19 07:51:48 +0800

    first commit

---
+++ src/main/java/arifrizal/restfull/BelajarSpringBootRestfullApiApplication.java
@@ -0,0 +1,13 @@
+package arifrizal.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringBootRestfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringBootRestfullApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arifrizal/restfull/BelajarSpringBootRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package arifrizal.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringBootRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
